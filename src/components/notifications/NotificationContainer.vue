9d3e9101bf6f4dcb188e001f55d5e697f0b563b7
Julien Zolo <julienz@perion.com>
2020-06-27 00:26:28 +0200

    [BUILD][NOTIF] Added notification components

---
+++ src/components/notifications/NotificationContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="notification-container">
+    <NotificationBar
+      v-for="notification in notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import NotificationBar from "@/components/notifications/NotificationBar.vue";
+
+export default {
+  name: "NotificationContainer",
+  computed: {
+    ...mapState({
+      notifications: state => state.notification.notifications
+    })
+  },
+  components: {
+    NotificationBar
+  }
+};
+</script>
+
+<style scoped>
+.notification-container {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding-right: 40px;
+}
+</style>
