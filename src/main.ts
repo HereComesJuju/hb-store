0ccb744345a3ae768e33bf3bc403a20e9ed2d6ef
Julien Zolo <julienz@perion.com>
2020-06-26 23:22:26 +0200

    [BUILD][SETUP] Installed dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Unicon from "vue-unicons";
+import { uniBars, uniMultiply, uniShoppingCart } from "vue-unicons/src/icons";
+
+Unicon.add([uniBars, uniMultiply, uniShoppingCart]);
+Vue.use(Unicon);
+
+import "./assets/styles/main.css";
 
 Vue.config.productionTip = false;
 
---
+++ tailwind.config.js
@@ -0,0 +1,107 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        "main-bg": "#f1f4f7"
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        sister: ["Love Ya Like A Sister", "cursive"],
+        "dylan-plain": ["akaDylan Plain", "cursive"],
+        "2p": ["Press Start 2P", "cursive"],
+        comica: ["Comica BD", "cursive"]
+      },
+      fontSize: {
+        "7xl": "5rem",
+        "8xl": "6rem",
+        "9xl": "7rem",
+        "10xl": "8rem"
+      },
+      height: {
+        half: "50%"
+      },
+      inset: {
+        "-full": "-100%",
+        "-half": "-50%",
+        "-xl": "-1280px",
+        "-lg": "-768px",
+        "-md": "-640px",
+        "-sm": "-320px",
+        "-16": "-4rem",
+        "-12": "-3rem",
+        "-10": "-2.5rem",
+        "-8": "-2rem",
+        "-6": "-1.5rem",
+        "-5": "-1.25rem",
+        "-4": "-1rem",
+        "-3": "-0.75rem",
+        "-2": "-0.5rem",
+        "-1": "-0.25rem",
+        "1": "0.25rem",
+        "2": "0.5rem",
+        "3": "0.75rem",
+        "4": "1rem",
+        "5": "1.25rem",
+        "6": "1.5rem",
+        "8": "2rem",
+        "10": "2.5rem",
+        "12": "3rem",
+        "16": "4rem",
+        sm: "320px",
+        md: "640px",
+        lg: "768px",
+        xl: "1280px",
+        half: "50%",
+        full: "100%"
+      },
+      maxHeight: {
+        "screen-xs": "425px",
+        "screen-sm": "640px",
+        "screen-md": "768px",
+        "screen-lg": "1024px",
+        "screen-xl": "1280px"
+      },
+      maxWidth: {
+        "screen-xs": "425px",
+        "screen-sm": "640px",
+        "screen-md": "768px",
+        "screen-lg": "1024px",
+        "screen-xl": "1280px",
+        "screen-xxl": "1680px"
+      },
+      screens: {
+        xs: { min: "425px", max: "639px" },
+        xxs: { max: "424px" }
+      },
+      width: {
+        "fit-content": "fit-content"
+      },
+      zIndex: {
+        "0": 0,
+        "10": 10,
+        "15": 15,
+        "20": 20,
+        "25": 25,
+        "30": 30,
+        "35": 35,
+        "40": 40,
+        "45": 45,
+        "50": 50,
+        "55": 55,
+        "60": 60,
+        "65": 65,
+        "70": 70,
+        "75": 75,
+        "80": 80,
+        "85": 85,
+        "90": 90,
+        "95": 95,
+        "100": 100,
+        top: 2147483647
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
